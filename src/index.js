2bd060b6ca103f2146580fab5c525a0f6c9cec85
David Neubauer <davidneub@gmail.com>
2016-05-15 16:08:01 +0200

    Add base webgl context render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import createApp from './app';
+function render(canvas) {
+  const gl = createWebGLContext(canvas);
 
-console.log(createApp());
+  if (gl) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  }
+}
+
+function createWebGLContext(canvas) {
+  try {
+    return canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+requestAnimationFrame(() => {
+  const canvas = document.getElementById("vanilla");
+  render(canvas);
+});
